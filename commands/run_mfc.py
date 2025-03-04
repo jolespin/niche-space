727e3075f1011ab5d1090dd224e6fa9f04ad4a6d
EC2 Default User <ec2-user@ip-172-16-53-87.us-west-2.compute.internal>
2025-03-04 23:47:22 +0000

    fixed run_mfc.py to fill 0

--- commands/run_mfc.py
+++ commands/run_mfc.py
@@ -5,8 +5,9 @@ import numpy as np # Can't install NumPy 2.2.2 which is what the pkls were saved
 import pandas as pd # 'v2.2.3'
 #import anndata as ad
 
-from ensemble_networkx import convert_network, read_parquet_nonredundant_pairwise_matrix
 
+from ensemble_networkx import convert_network, read_parquet_nonredundant_pairwise_matrix
+from pyexeggutor import build_logger
 
 # Metabolic Niche Space
 from nichespace.neighbors import (
@@ -23,10 +24,16 @@ output_directory=f"../data/cluster/mfc/v2025.3.3/{quality_label}/"
 os.makedirs(output_directory, exist_ok=True)
 
 #jaccard_distances = pd.read_pickle(f"../data/cluster/mfc/{quality_label}/genomic_traits_clusterani.jaccard_distance.dataframe.pkl")
-
-jaccard_distances = read_parquet_nonredundant_pairwise_matrix(f"{output_directory}/genomic_traits_clusterani.jaccard_distance.nonredundant.parquet")
-jaccard_distances = convert_network(jaccard_distances, pd.DataFrame)
-
+logger = build_logger("MFC Runner", stream=sys.stderr)
+logger.info("Reading parquet")
+jaccard_distances = pd.read_parquet(f"{output_directory}/genomic_traits_clusterani.jaccard_distance.redundant.parquet")
+# jaccard_distances = read_parquet_nonredundant_pairwise_matrix(f"{output_directory}/genomic_traits_clusterani.jaccard_distance.nonredundant.parquet")
+#logger.info("Converting non-redundant to redundant form")
+#jaccard_distances = convert_network(jaccard_distances, pd.DataFrame)
+#np.fill_diagonal(jaccard_distances.values, 0)
+logger.info("Forcing symmetry")
+jaccard_distances = (jaccard_distances + jaccard_distances.T)/2
+logger.info("Starting KNeighborsLeidenClustering")
 n = jaccard_distances.shape[0]
 n_neighbors_params = [int, int(np.log(n)), int(np.sqrt(n)/2)]
 clusterer = KNeighborsLeidenClustering(
@@ -35,16 +42,18 @@ clusterer = KNeighborsLeidenClustering(
     observation_type="ani-cluster", 
     class_type="MFC", 
     n_neighbors=n_neighbors_params, 
-    n_trials=25, 
+    #n_trials=25, 
     n_jobs=-1,
-    n_iter=100, 
+    #n_iter=100, 
+    n_trials=1,
+    n_iter=5,
     cluster_prefix="MFC-",
     verbose=4,
         
 )
 clusterer.fit(jaccard_distances)
 clusterer.to_file(f"{output_directory}/{clusterer.name}.KNeighborsLeidenClustering.pkl")    
-
+logger.info("Finished")
 
 
 
--- sync.sh
+++ sync.sh
@@ -1,1 +1,1 @@
-aws s3 sync s3://newatlantis-scripts-prod/jolespin_development/MetabolicNicheSpace/working/niche-space/data/ data/ --delete 
+aws s3 sync s3://newatlantis-scripts-prod/jolespin_development/MetabolicNicheSpace/working/niche-space/data/ data/ --delete
