49d06c671be606594c9e79b411756557b8a961a1
EC2 Default User <ec2-user@ip-172-16-40-228.us-west-2.compute.internal>
2025-03-11 05:54:43 +0000

    updated annotate commands

---
+++ commands/logs/annotator.e
@@ -0,0 +1,26 @@
+2025-03-11 05:51:46,521 - pyexeggutor - INFO - Loading MNS
+2025-03-11 05:52:42,555 - pyexeggutor - INFO - Annotating embeddings
+Command terminated by signal 9
+	Command being timed: "python run_annotator.py"
+	User time (seconds): 8.03
+	System time (seconds): 26.47
+	Percent of CPU this job got: 43%
+	Elapsed (wall clock) time (h:mm:ss or m:ss): 1:18.69
+	Average shared text size (kbytes): 0
+	Average unshared data size (kbytes): 0
+	Average stack size (kbytes): 0
+	Average total size (kbytes): 0
+	Maximum resident set size (kbytes): 31810532
+	Average resident set size (kbytes): 0
+	Major (requiring I/O) page faults: 329
+	Minor (reclaiming a frame) page faults: 7569580
+	Voluntary context switches: 37794
+	Involuntary context switches: 6533
+	Swaps: 0
+	File system inputs: 19571736
+	File system outputs: 24
+	Socket messages sent: 0
+	Socket messages received: 0
+	Signals delivered: 0
+	Page size (bytes): 4096
+	Exit status: 0
--- commands/run_annotator.py
+++ commands/run_annotator.py
@@ -2,6 +2,7 @@ import os
 import sys
 from pyexeggutor import (
     read_pickle,
+    build_logger,
 )
 
 # Metabolic Niche Space
@@ -11,14 +12,17 @@ from nichespace.manifold import (
     DEFAULT_REGRESSOR_PARAM_SPACE,
 )
 
+logger = build_logger(stream=sys.stderr)
 # Load data
 # quality_label="completeness_gte90.contamination_lt5"
+logger.info("Loading MNS")
 quality_label="completeness_gte50.contamination_lt10"
-model_name="NAL-GDB_MNS_v3.SLC-MFC.medium"
-mns = read_pickle(f"../data/manifold/{quality_label}/{model_name}.HierarchicalNicheSpace.pkl")
-output_directory=f"../data/annotate/{quality_label}"
-os.makedirs(output_directory, exists_ok=True)
+model_name="NAL-GDB_MNS__v2025.3.3__SLC-MFC.medium"
+mns = read_pickle(f"../data/manifold/v2025.3.3/{quality_label}/{model_name}.HierarchicalNicheSpace.pkl")
+output_directory=f"../data/annotate/v2025.3.3/{quality_label}"
+os.makedirs(output_directory, exist_ok=True)
 
+logger.info("Annotating embeddings")
 # Annotate
 annotator = EmbeddingAnnotator(
     name=mns.name,
@@ -27,12 +31,12 @@ annotator = EmbeddingAnnotator(
     embedding_type="MNS",
     estimator=DEFAULT_REGRESSOR, 
     param_space=DEFAULT_REGRESSOR_PARAM_SPACE, 
-    n_trials=1000, 
+    n_trials=500, 
     n_iter=10, 
     n_concurrent_trials=1, 
     n_jobs=-1,
     verbose=3,
-    checkpoint_directory=f"../data/annotate/{quality_label}/checkpoints",
+    checkpoint_directory=f"../data/annotate/v2025.3.3/{quality_label}/checkpoints",
 )
 
 annotator.fit(
@@ -41,5 +45,6 @@ annotator.fit(
     X_testing=mns.X1_.astype(int),
     Y_testing=mns.diffusion_coordinates_grouped_,
 )
-
-annotator.to_file(f"../data/annotate/{quality_label}/{model_name}.EmbeddingAnnotator.pkl")
+logger.info("Writing file")
+annotator.to_file(f"../data/annotate/v2025.3.3/{quality_label}/{model_name}.EmbeddingAnnotator.pkl")
+logger.info("Complete")
